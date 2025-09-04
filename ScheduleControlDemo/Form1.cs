065289a63798f0403fbee783b33ba3ac8020e759
Rabina4363sf <rabina.murugan@syncfusion.com>
2025-09-04 18:19:08 +0530

    KB-21183-Sample, README.md and image updated

---
+++ ScheduleControlDemo/Form1.cs
@@ -0,0 +1,55 @@
+﻿using GridScheduleSample;
+using Syncfusion.Schedule;
+using Syncfusion.Windows.Forms.Schedule;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ScheduleControlDemo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.Load += Form1_Load;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SimpleScheduleDataProvider data;
+
+            if (File.Exists("default.schedule"))
+            {
+                data = SimpleScheduleDataProvider.LoadBinary("default.schedule");
+                data.FileName = "default.schedule";
+            }
+
+            else
+            {
+                data = new SimpleScheduleDataProvider();
+                data.MasterList = new SimpleScheduleAppointmentList();
+                data.FileName = "default.schedule";
+            }
+            this.scheduleControl1.ScheduleType = ScheduleViewType.Day;
+            ScheduleAppointment item = data.MasterList.NewScheduleAppointment() as ScheduleAppointment;
+            item.StartTime = DateTime.Now;
+            item.EndTime = item.StartTime.AddDays(2);
+
+            //item.BackColor = Color.Green;
+            item.ForeColor = Color.Red;
+            data.MasterList.Add(item);
+           
+            this.scheduleControl1.DataSource = data;
+           
+            this.scheduleControl1.Appearance.DivisionsPerHour = 4;
+        }
+    }
+}
---
+++ ScheduleControlDemo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ScheduleControlDemo
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("MDAxQDMyMzQyZTMwMmUzMGJoUUsva0l0TXhFa040TEk0NHRXcE9RWmIyOXBpaEszcU95cEpsU3hEMzA9");
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
